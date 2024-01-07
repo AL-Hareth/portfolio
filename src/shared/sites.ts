6b6185637d9fce91eb1ca63a81be994a3030b95d
AL-Hareth <alharethturab@gmail.com>
2024-01-07 19:50:15 +0300

    added two websites

--- src/shared/sites.ts
+++ src/shared/sites.ts
@@ -10,7 +10,19 @@ export const sites: ProjectData[] = [
     {
         name: "Teryaq",
         description: "Website for a group of pharmacy students in the university of jordan",
-        link: "https://teryaqq.vercel.app",
+        link: "https://teryaq.info",
         imageUrl: "/teryaq.png"
+    },
+    {
+        name: "Premium Store",
+        description: "A drop shipping platform for sellers who can't store, and ship.",
+        link: "https://premium-store.website",
+        imageUrl: "/premium-store.png"
+    },
+    {
+        name: "Larablog",
+        description: "A small microblogging platform built with laravel",
+        link: "https://larablog.alhareth.xyz",
+        imageUrl: "/larablog.png"
     },
 ];
